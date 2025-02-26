78f33d22539666dbf412c7f809ca068737246988
Ilker Kalecik <ikalecik26313@gmail.com>
2025-02-26 18:19:17 +0300

    Logo ekleme özelliği eklendi

---
+++ src/main/java/com/example/cafemenu/entity/Logo.java
@@ -0,0 +1,20 @@
+package com.example.cafemenu.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Logo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String logoUrl;
+
+
+}
